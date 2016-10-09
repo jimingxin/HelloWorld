3e6387efb2625c8618f3b7c0059604d75f5edec2
嵇明新 <493972613@qq.com>
2016-10-09 17:55:25 +0800

    命令行github

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 嵇明新 on 2016/10/9.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by 嵇明新 on 2016/10/9.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
