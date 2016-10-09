0532706a6b95862dcc808bcdcbee736dd1f911d5
嵇明新 <493972613@qq.com>
2016-10-09 18:15:32 +0800

    新增了NextViewContrller

---
+++ HelloWorld/NextViewController.h
@@ -0,0 +1,13 @@
+//
+//  NextViewController.h
+//  HelloWorld
+//
+//  Created by 嵇明新 on 2016/10/9.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextViewController : UIViewController
+
+@end
---
+++ HelloWorld/NextViewController.m
@@ -0,0 +1,37 @@
+//
+//  NextViewController.m
+//  HelloWorld
+//
+//  Created by 嵇明新 on 2016/10/9.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+#import "NextViewController.h"
+
+@interface NextViewController ()
+
+@end
+
+@implementation NextViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
